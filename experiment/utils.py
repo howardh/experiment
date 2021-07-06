f1343fa6fe85d52b5d29b03fd7ebe49ec0bc5b68
Howard <howard.huang@mail.mcgill.ca>
2021-07-06 17:08:52 -0400

    Implement pickling function that handles torch objects

--- experiment/utils.py
+++ experiment/utils.py
@@ -1,7 +1,9 @@
 import os
 import itertools
-from typing import List, Any
+from typing import List, Any, Mapping, BinaryIO, Iterable
+import io
 
+import torch
 import dill
 
 def find_next_free_file(prefix, suffix, directory):
@@ -80,3 +82,79 @@ def dill_load_rolling(filenames : List[str]):
             # Delete corrupted files so they don't overwrite valid saves
             os.remove(filename)
     raise Exception('Unable to find a valid file to load.')
+
+##################################################
+# Pickling
+##################################################
+
+MAGIC_NUMBER = 'd36f3c96-e6f8-417c-be78-26275b53ccad'
+
+def _can_dump(obj):
+    """ Check if the object can be dumped as is, or if it requires extra handling before dumping. """
+    if isinstance(obj,torch.nn.Module):
+        return False
+    if isinstance(obj,torch.optim.Optimizer):
+        return False
+    if isinstance(obj,Mapping):
+        return all((_can_dump(v) for v in obj.values()))
+    if isinstance(obj,str): # str is also an Iterable, so catch it before that branch
+        return True
+    if isinstance(obj,Iterable):
+        return all((_can_dump(v) for v in obj))
+    return True
+
+def dump(obj : Any, f : BinaryIO):
+    """ Pickle objects with torch object handling. """
+    VERSION = 1
+    def dump_rec(obj : Any, f : BinaryIO):
+        if _can_dump(obj):
+            dill.dump({'type': 'raw'}, f)
+            dill.dump(obj, f)
+        elif isinstance(obj,dict):
+            keys = list(obj.keys())
+            dill.dump({'type': 'dict', 'keys': keys}, f)
+            for k in keys:
+                dump_rec(obj[k], f)
+        elif isinstance(obj,list):
+            dill.dump({'type': 'list', 'length': len(obj)}, f)
+            for o in obj:
+                dump_rec(o, f)
+        elif isinstance(obj,torch.nn.Module) or isinstance(obj,torch.optim.Optimizer):
+            dill.dump({'type': 'torch_state_dict'}, f)
+            buff = io.BytesIO()
+            torch.save(obj.state_dict(),buff)
+            buff.seek(0)
+            byte_str = buff.read()
+            dill.dump(byte_str, f)
+        else:
+            raise NotImplementedError('Unable to handle object of type %s.' % type(obj)) # pragma: no cover
+    dill.dump([MAGIC_NUMBER,VERSION], f)
+    dump_rec(obj, f)
+
+def load(f : BinaryIO, torch_map_location=None):
+    VERSION = 1
+    header = dill.load(f)
+    if header[0] != MAGIC_NUMBER:
+        raise Exception('invalid file format.')
+    if header[1] != VERSION:
+        raise Exception('Unable to handle file version.')
+    def load_rec(f : BinaryIO):
+        meta = dill.load(f)
+        if not isinstance(meta,Mapping):
+            raise Exception('Invalid file format')
+        if meta['type'] == 'raw':
+            return dill.load(f)
+        elif meta['type'] == 'dict':
+            keys = meta['keys']
+            return {k:load_rec(f) for k in keys}
+        elif meta['type'] == 'list':
+            length = meta['length']
+            return [load_rec(f) for _ in range(length)]
+        elif meta['type'] == 'torch_state_dict':
+            byte_str = dill.load(f)
+            buff = io.BytesIO(byte_str)
+            buff.seek(0)
+            return torch.load(buff, map_location=torch_map_location)
+        else:
+            raise Exception('Invalid file format')
+    return load_rec(f)
--- tests/utils/test_dill_rolling.py
+++ tests/utils/test_dill_rolling.py
@@ -1,6 +1,11 @@
 import os
+from typing import Mapping
 
-from experiment.utils import dill_dump_rolling, dill_load_rolling
+import pytest
+import torch
+import dill
+
+from experiment.utils import dill_dump_rolling, dill_load_rolling, dump, load
 
 def test_rolling_save(tmpdir):
     directory = tmpdir.mkdir('output')
@@ -80,10 +85,10 @@ def test_rolling_save_fills_missing_slots(tmpdir):
 
     assert len(os.listdir(directory)) == 0
 
-    obj1 = {'a': 1, 'b': 2}
+    obj1 = {'a': 1, 'b': 2        }
     obj2 = {'a': 1, 'b': 2, 'c': 3}
-    obj3 = {'b': 2, 'c': 3}
-    obj4 = {'a': 1, 'c': 3}
+    obj3 = {        'b': 2, 'c': 3}
+    obj4 = {'a': 1,         'c': 3}
     dill_dump_rolling(obj1, filenames)
     dill_dump_rolling(obj2, filenames)
     dill_dump_rolling(obj3, filenames)
@@ -123,3 +128,134 @@ def test_rolling_save_fills_missing_slots(tmpdir):
     os.remove(filenames[1])
     obj = dill_load_rolling(filenames)
     assert obj == obj1
+
+@pytest.mark.parametrize('obj',[
+    1,
+    'b',
+    'asdf',
+    r'asdf',
+    b'123',
+    [],
+    [1,2,3],
+    [1,2,'foo'],
+    {},
+    {'a': 1},
+    {'a': [1,'foo'], 'b': {'c': 5, 'd': 'asdf'}},
+])
+def test_dump_primitives(obj, tmpdir):
+    directory = tmpdir.mkdir('output')
+    filename = os.path.join(directory,'file')
+    with open(filename,'wb') as f:
+        dump(obj,f)
+    with open(filename,'rb') as f:
+        loaded_obj = load(f)
+    assert loaded_obj == obj
+
+def test_dump_torch_in_dict(tmpdir):
+    directory = tmpdir.mkdir('output')
+    filename = os.path.join(directory,'file')
+    class Network(torch.nn.Module):
+        def __init__(self, num_actions):
+            super().__init__()
+            self.conv = torch.nn.Sequential(
+                torch.nn.Conv2d(
+                    in_channels=4,out_channels=16,kernel_size=8,stride=4),
+                torch.nn.ReLU(),
+                torch.nn.Conv2d(
+                    in_channels=16,out_channels=16,kernel_size=4,stride=2),
+                torch.nn.ReLU()
+            )
+            self.fc = torch.nn.Sequential(
+                torch.nn.Linear(in_features=16*9*9,out_features=128),
+                torch.nn.ReLU(),
+                torch.nn.Linear(in_features=128,out_features=num_actions),
+            )
+        def forward(self, obs):
+            x = obs
+            x = self.conv(x)
+            x = x.view(-1,9*9*16)
+            x = self.fc(x)
+            return x
+    net = Network(5)
+    optim = torch.optim.Adam(net.parameters(),lr=1e-3)
+    with open(filename,'wb') as f:
+        dump({'net': net, 'optim': optim},f)
+    with open(filename,'rb') as f:
+        loaded_obj = load(f)
+
+    assert isinstance(loaded_obj,Mapping)
+
+    original_state_dict = net.state_dict()
+    loaded_state_dict = loaded_obj['net']
+    assert loaded_state_dict.keys() == original_state_dict.keys()
+    for k in loaded_state_dict.keys():
+        assert (loaded_state_dict[k] == original_state_dict[k]).all()
+
+    original_state_dict = optim.state_dict()
+    loaded_state_dict = loaded_obj['optim']
+    assert loaded_state_dict.keys() == original_state_dict.keys()
+    for k in loaded_state_dict.keys():
+        assert loaded_state_dict[k] == original_state_dict[k]
+
+def test_dump_torch_in_list(tmpdir):
+    directory = tmpdir.mkdir('output')
+    filename = os.path.join(directory,'file')
+    class Network(torch.nn.Module):
+        def __init__(self, num_actions):
+            super().__init__()
+            self.conv = torch.nn.Sequential(
+                torch.nn.Conv2d(
+                    in_channels=4,out_channels=16,kernel_size=8,stride=4),
+                torch.nn.ReLU(),
+                torch.nn.Conv2d(
+                    in_channels=16,out_channels=16,kernel_size=4,stride=2),
+                torch.nn.ReLU()
+            )
+            self.fc = torch.nn.Sequential(
+                torch.nn.Linear(in_features=16*9*9,out_features=128),
+                torch.nn.ReLU(),
+                torch.nn.Linear(in_features=128,out_features=num_actions),
+            )
+        def forward(self, obs):
+            x = obs
+            x = self.conv(x)
+            x = x.view(-1,9*9*16)
+            x = self.fc(x)
+            return x
+    net = Network(5)
+    optim = torch.optim.Adam(net.parameters(),lr=1e-3)
+    with open(filename,'wb') as f:
+        dump([net, optim],f)
+    with open(filename,'rb') as f:
+        loaded_obj = load(f)
+
+    assert isinstance(loaded_obj,list)
+
+    original_state_dict = net.state_dict()
+    loaded_state_dict = loaded_obj[0]
+    assert loaded_state_dict.keys() == original_state_dict.keys()
+    for k in loaded_state_dict.keys():
+        assert (loaded_state_dict[k] == original_state_dict[k]).all()
+
+    original_state_dict = optim.state_dict()
+    loaded_state_dict = loaded_obj[1]
+    assert loaded_state_dict.keys() == original_state_dict.keys()
+    for k in loaded_state_dict.keys():
+        assert loaded_state_dict[k] == original_state_dict[k]
+
+@pytest.mark.parametrize('data',[
+    [['invalid magic number',1]],
+    [['d36f3c96-e6f8-417c-be78-26275b53ccad',-1]],
+    [['d36f3c96-e6f8-417c-be78-26275b53ccad',1],'foo'],
+    [['d36f3c96-e6f8-417c-be78-26275b53ccad',1],{'type': 'invalid type'}],
+])
+def test_load_invalid_formats(data,tmpdir):
+    directory = tmpdir.mkdir('output')
+    filename = os.path.join(directory,'file')
+
+    with open(filename,'wb') as f:
+        for x in data:
+            dill.dump(x,f)
+    with open(filename,'rb') as f:
+        with pytest.raises(Exception):
+            load(f)
